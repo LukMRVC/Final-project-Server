ffeb131294ac61cd4c2f3c3d595d367cc63243a8
Lukas Moravec <sydnexpklover@gmail.com>
2017-06-28 14:38:56 +0200

    Initial commit

---
+++ final_project/CategoryDialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Gtk;
+using System.Text.RegularExpressions;
+
+namespace final_project
+{
+	public partial class CategoryDialog : Gtk.Dialog
+	{
+
+		public string name
+		{
+			get;
+			private set;
+		}
+
+		public CategoryDialog()
+		{
+			this.Build();
+		}
+
+		protected void Cancelled(object sender, EventArgs e)
+		{
+			this.name = null;
+			this.Destroy();
+			return;
+		}
+
+		protected void buttonOkClicked(object sender, EventArgs e)
+		{
+			this.name = this.nameEntry.Text;
+			if (string.IsNullOrWhiteSpace(name))
+			{
+				MessageDialog dlg = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close, "Název nesmí být prázdný!");
+				dlg.Run();
+				dlg.Destroy();
+				return;
+			}
+			else if( !(new Regex(@"^(\p{L}+\s?)+$").IsMatch(name)) ) { 
+				MessageDialog dlg = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Close, "Název nesmí obsahovat čísla a speciální znaky!");
+				dlg.Run();
+				dlg.Destroy();
+				return;
+			}
+            this.Destroy();
+		}
+	}
+}
---
+++ final_project/MainWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Gtk;
+using final_project;
+
+public partial class MainWindow : Gtk.Window
+{
+
+	//private Server serv;
+
+	private Server server;
+
+	public MainWindow(Server s) : base(Gtk.WindowType.Toplevel)
+	{
+		this.server = s;
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void addCategory(object sender, EventArgs e)
+	{
+		string name = new CategoryDialog().name;
+	}
+
+	protected void connectToDatabaseAction(object sender, EventArgs e)
+	{
+	}
+
+	protected void databaseConnectionFormAction(object sender, EventArgs e)
+	{
+		string databaseConnectionString = new DatabaseConnectionForm().connectionString;
+	}
+}
