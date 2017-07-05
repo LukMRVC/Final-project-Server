ef2d932d6be7d3a7183b2a0eda3f1f1f36d2b819
Lukas Moravec <sydnexpklover@gmail.com>
2017-07-05 22:13:53 +0200

    Database connection
    Finished database connection form and it's now able to connect to databasem, synchronously I'm afraid

---
+++ final_project/DatabaseConnectionDialog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace final_project
+{
+	public partial class DatabaseConnectionDialog : Gtk.Dialog
+	{
+		public DatabaseConnectionDialog()
+		{
+			this.Build();
+		}
+	}
+}
---
+++ final_project/gtk-gui/final_project.DatabaseConnectionDialog.cs
@@ -0,0 +1,14 @@
+
+namespace final_project
+{
+	public partial class DatabaseConnectionDialog
+	{
+		private Gtk.Button buttonCancel;
+
+		private Gtk.Button buttonOk;
+
+		private void Build()
+		{
+		}
+	}
+}
