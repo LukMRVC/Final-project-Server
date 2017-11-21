134eb1e02f22d00bf0645a27e97e23c9f9cffe09
Lukas Moravec <sydnexpklover@gmail.com>
2017-11-21 18:28:58 +0100

    Added icon

---
+++ final_project/gtk-gui/final_project.OrderHistory.cs
@@ -0,0 +1,14 @@
+
+namespace final_project
+{
+	public partial class OrderHistory
+	{
+		private Gtk.ScrolledWindow GtkScrolledWindow;
+
+		private Gtk.NodeView nodeview2;
+
+		private void Build()
+		{
+		}
+	}
+}
